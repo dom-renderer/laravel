c20dd64bf35e64b70591547bd156bb6b9b60fd57
dom-renderer <dr.jainilsoni@gmail.com>
2024-08-18 21:07:04 +0530

    Create 2024_08_16_170048_create_attendances_table.php

---
+++ attendance-system/2024_08_16_170048_create_attendances_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('date');
+            $table->dateTime('io_time');
+            $table->tinyInteger('type')->comment('0 = NONE (I/O) BREAK | 1 = CHECK IN | 2 = CHECK OUT');
+            $table->tinyInteger('break_type')->comment('0 = NONE (I/O) CHECK | 1 = BREAK IN | 2 = BREAK OUT');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
